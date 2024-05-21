d67bdae5985ee152d20a482723ca521e647f0784
Ramlal Nikam <chiragnikam02@gmail.com>
2024-05-21 19:13:46 +0530

    project set-up

---
+++ src/main/java/com/development/api/SevaSahyog/SevaSahyogApplication.java
@@ -0,0 +1,13 @@
+package com.development.api.SevaSahyog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevaSahyogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevaSahyogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/development/api/SevaSahyog/SevaSahyogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.api.SevaSahyog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevaSahyogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
